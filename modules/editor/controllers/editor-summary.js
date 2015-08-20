fa93819c892b4e0a0dc19c39f25c98ba1755a294
Eric McElyea <emcelyea@horizon.csueastbay.edu>
2015-08-20 14:46:54 -0700

    new functional less ugly version of summary page

--- modules/editor/controllers/editor-summary.js
+++ modules/editor/controllers/editor-summary.js
@@ -1,17 +1,21 @@
 'use strict';
 
 angular.module('editor')
-	.controller('EditorSummaryController',['$scope', 'DataService', '$http',
-		function($scope, DataService, $http){
+	.controller('EditorSummaryController',['$scope', 'DataService', '$http', '$timeout',
+		function($scope, DataService, $http, $timeout){
 
-			$scope.show = {};
+			$scope.show = {}; //control what can be seen currently
 			$scope.show.routes = {};
 			$scope.show.models = {};
+			$scope.show.uptime = {};
+			$scope.show.rebuild = {};
+			$scope.show.republish = {};
 			$scope.show.models.properties = {};
-			$scope.show.models.schema = {};
+			$scope.show.models.schemas = {};
 			$scope.restRoutes = [];
 			$scope.schemas = [];
-			$scope.models;
+		
+			$scope.windowHeight = $(window).height() - 40;
 			$http.get('/admin/restRoutes')
 				.then(function(response){
 					for(var x in response.data){
@@ -26,10 +30,62 @@ angular.module('editor')
 			$http.get('/admin/models')
 				.then(function(response){
 					for(var x in response.data){
-						$scope.schemas.push({name:x, data:response.data[x]});
+						$scope.schemas.push({name:x, 
+											properties:response.data[x].properties, 
+											schema:response.data[x].schema});
 					
 					}
-					console.log($scope.schemas);
 				})
+
+			$http.get('/admin/uptime')
+				.then(function(response){
+					$scope.uptime = {}
+					$scope.uptime.hours = Math.floor(response.data.uptime/3600);
+					$scope.uptime.mins = Math.floor((response.data.uptime % 3600)/60);
+					$scope.uptime.secs = Math.floor(response.data.uptime % 60);
+				})
+
+			$scope.swapCurrentView = function(view){
+				if($scope.show[view].show){
+					$scope.show[view].show = false;
+				}else{
+					for(var x in $scope.show){
+						$scope.show[x].show = false;
+					}
+					$scope.show[view].show = true;
+				}
+				if(view === 'uptime'){
+					$http.get('/admin/uptime')
+					.then(function(response){
+						$scope.uptime.hours = Math.floor(response.data.uptime/3600);
+						$scope.uptime.mins = Math.floor((response.data.uptime % 3600)/60);
+						$scope.uptime.secs = Math.floor(response.data.uptime % 60);	
+						moveClock();
+					})	
+				}
+			}
+
+			var moveClock = function(){
+				if($scope.uptime.secs + 1 === 60){
+					$scope.uptime.secs = 0;
+					$scope.uptime.mins++;
+				}else{
+					$scope.uptime.secs++;
+				}
+				$timeout(moveClock,1000);	
+			}
+
+			$scope.republish = function(){
+				$http.get('/admin/republish')
+				.then(function(response){
+					$scope.republish = response;
+				})
+			}
+			$scope.rebuild = function(){
+				$http.get('/admin/rebuild')
+				.then(function(response){
+					$scope.rebuild = response;
+				})
+			}
 		}
 	]) 
