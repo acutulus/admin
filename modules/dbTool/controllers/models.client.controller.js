ca5a3658577adeb92d06b7b011b9776fab2349b9
Eric McElyea <emcelyea@horizon.csueastbay.edu>
2015-09-30 16:57:28 -0700

    redoing models view in admin

--- modules/dbTool/controllers/models.client.controller.js
+++ modules/dbTool/controllers/models.client.controller.js
@@ -3,12 +3,28 @@
 angular.module('dbtools').controller('ModelsCtrl', ['$scope','$http','$stateParams',
 	function($scope, $http, $stateParams){
 		$scope.modelName = $stateParams.tablename;
-
+		$scope.showJSON = {};
+		$scope.displayTable = true;
 		$http.get('/admin/models')
 			.then(function(response){
 				console.log(response);
-					$scope.schema = response.data[$stateParams.tablename.slice(0, $stateParams.tablename.length - 1)];
+				$scope.model = response.data[$stateParams.tablename]
 			})
 
+		$scope.buildJSON = true;
+		$scope.displayJSON = function(){
+			
+			if($scope.displayTable){
+				$scope.displayTable = false;
+				if($scope.buildJSON){
+					var str = JSON.stringify($scope.model, undefined, 4);
+					$scope.jsonDisplayData = str;
+					$scope.buildJSON = false;
+				}
+			}else{
+				$scope.displayTable = true;
+			}
+		}
 	}
+	
 ])
