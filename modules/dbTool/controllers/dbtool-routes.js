d12265f979557f92977cb240d16ebad36afcdd42
ericmcer <emcelyea@horizon.csueastbay.edu>
2016-01-18 15:17:27 -0800

    adding simple version of route tester

--- modules/dbTool/controllers/dbtool-routes.js
+++ modules/dbTool/controllers/dbtool-routes.js
@@ -7,12 +7,40 @@ angular.module('dbtools').controller('RoutesCtrl', ['$scope','$stateParams','$ht
 			alert("No permissions");
 			location.href = "/admin/dbtools/signin";
 		}
+
 		$scope.routeName = $stateParams.tablename;
 		$scope.routes;
+		$scope.routeTesting = {};
+
 		$http.get('/admin/restRoutes')
 		.then(function(response){
 			console.log(response);
 			$scope.routes = response.data[$scope.routeName].functions;
-		})
+		});
+
+		$scope.routeTestable = function(key){
+			if(typeof key === 'string'){		
+				if(key.slice(0,3) === "get"){
+					return true;
+				}
+				if(key.slice(0,4) === "read"){
+					return true;
+				}
+				if(key.slice(0,5) === "query"){
+					return true;
+				}
+			}
+			return false;
+		}
+
+		$scope.testRoute = function(key, val){
+			$scope.routeTesting[key] = {testing:true, loading:true};
+			$http.get(val)
+			.then(function(data){
+				$scope.routeTesting[key].data = data;
+				console.log(data);
+				$scope.routeTesting[key].loading = false;
+			})
+		}
 	}
 ]);
